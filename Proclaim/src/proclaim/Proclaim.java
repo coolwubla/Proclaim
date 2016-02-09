1f3ab0471e9f1cb62614b3e341e1a5b753241342
bw372p <bw372p@NJCDTL01bw372p.ITServices.sbc.com>
2016-02-09 16:33:40 -0500

    Hey

--- Proclaim/src/proclaim/Proclaim.java
+++ Proclaim/src/proclaim/Proclaim.java
@@ -15,7 +15,7 @@ public class Proclaim {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello Paul");
     }
     
 }
